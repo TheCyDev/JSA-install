41d76bda680c4d7c092ad3b7b5ed88a3f964b36f
r3g0Developer <71458907+r3g0Developer@users.noreply.github.com>
2020-10-28 18:42:19 +0500

    Download

---
+++ djsa.js
@@ -0,0 +1,22 @@
+// Start //
+const Discord = require('discord.js');
+const loging = console.log; // console
+
+const instl = require; // installing
+// {
+// let appis;
+		//	const apis = ["https://vacefron.nl/api/", "https://api.alexflipnote.dev/"];
+			
+		//	const aps = Math.floor(Math.random() * apis.length);
+		//	const jons = apis[aps].join(" / ");
+			
+	//	message.channel.send(`\`${jons}\``);
+// }
+
+const timeout = setTimeout;
+
+				const interval = setInterval;
+				
+		// const start = module.exports;
+		
+				// end //
